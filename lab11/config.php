b41d1be2e87ff72cfd4e9b1389f9d0fb1711e9fc
3239250471 <61495319+3239250471@users.noreply.github.com>
2020-06-14 00:56:53 +0800

    Add files via upload

---
+++ lab11/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBNAME', 'login');
+define('DBUSER', '´úÇ¬');
+define('DBPASS', 'Dai05043159');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login')
+?>
